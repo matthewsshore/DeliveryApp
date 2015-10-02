61d353e696ad820d1ec8483590477692258190da
matt <matthew.sshore@gmail.com>
2015-10-02 12:42:20 -0700

    First import of files

--- app/src/main/java/com/example/skantoro/deliveryapp_testing/MainActivity.java
+++ app/src/main/java/com/example/skantoro/deliveryapp_testing/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         View.OnClickListener {
-    // TEST
+
 
     private static final String TAG = "MainActivity";
 
